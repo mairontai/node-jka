4b5ca7c871e7e155dcab4e88a4c3c80f15161900
mai <mai>
2020-08-06 21:15:54 +0300

    update listservers

--- node/modules/jkaMonitoring/commands/listservers.js
+++ node/modules/jkaMonitoring/commands/listservers.js
@@ -1,3 +1,8 @@
+const Embed = require("discord.js").MessageEmbed;
+const getDiscordId = require("../../../../utils/getDiscordId.js");
+
+const emb = new Embed();
+
 module.exports = function ({discord, db}) {
     discord.on("message", async message => {
         const content = message.content;
@@ -5,17 +10,32 @@ module.exports = function ({discord, db}) {
             const ref = db.database().ref(message.guild.name);
             const serversList = await ref.child("servers").once("value");
 
-            //let index = 1;
             let messageText = "";
-
             let counter = 0;
+
             for (let x in serversList.val()) {
+                let channel = discord.channels.cache.get(serversList.val()[x].textChannelId);
+
+                if (channel === undefined) {
+                    message.reply(`something wrong with the text channel!!! (${serversList.val()[x].name})`);
+                    continue;
+                }
+
+                messageText += channel.toString()
+                messageText += " : "
                 messageText += ++counter;
                 messageText += ") ";
                 messageText += serversList.val()[x].name;
+                messageText += " (`";
+                messageText += serversList.val()[x].ip;
+                messageText += "`)";
                 messageText += "\n";
             }
-            message.channel.send(messageText);
+
+            emb.setTitle("Servers list");
+            emb.setDescription(messageText);
+
+            message.channel.send(emb);
         }
     });
 };
